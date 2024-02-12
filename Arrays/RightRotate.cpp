4c753a419542fe95d1f59d4f764b138e58dfd8fe
Pavan-B-N <114333724+Pavan-B-N@users.noreply.github.com>
2024-02-12 18:46:26 +0530

    Solved right rotate array ,leetcode problem

---
+++ Arrays/RightRotate.cpp
@@ -0,0 +1,30 @@
+/*
+https://leetcode.com/problems/rotate-array/description/?envType=study-plan-v2&envId=top-interview-150
+
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+void reverseNum(vector<int> &nums, int start, int end)
+{
+    while (start < end)
+    {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
+void rotate(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    k %= n;
+    reverseNum(nums, 0, n - 1);
+    reverseNum(nums, 0, k - 1);
+    reverseNum(nums, k, n - 1);
+}
