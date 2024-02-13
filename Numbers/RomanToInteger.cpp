041cf131ca05b079045db078b8c2e673f660bdf3
Pavan-B-N <114333724+Pavan-B-N@users.noreply.github.com>
2024-02-13 14:17:37 +0530

    solved roman to integer leetcode problem

---
+++ Numbers/RomanToInteger.cpp
@@ -0,0 +1,83 @@
+/*
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+Input: s = "III"
+Output: 3
+
+Input: s = "LVIII"
+Output: 58
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int valueOfRoman(char ch)
+{
+    switch (ch)
+    {
+    case 'I':
+        return 1;
+    case 'V':
+        return 5;
+    case 'X':
+        return 10;
+    case 'L':
+        return 50;
+    case 'C':
+        return 100;
+    case 'D':
+        return 500;
+    case 'M':
+        return 1000;
+    default:
+        return 0;
+    }
+}
+/*
+traverse in reverse order
+if(current>pre){
+    result+=current;
+}else{
+    result-=current;
+}
+*/
+int romanToInteger(string s)
+{
+    int result = 0;
+    int prevValue = 0;
+
+    for (int i = s.length() - 1; i >= 0; --i)
+    {
+        int current = valueOfRoman(s[i]);
+
+        if (current < prevValue)
+        {
+            result -= current;
+        }
+        else
+        {
+            result += current;
+        }
+
+        prevValue = current;
+    }
+
+    return result;
+}
+int main()
+{
+    string str;
+    str = "LVIII"; // 58
+
+    int res = romanToInteger(str);
+    cout << "Value of roman '" << str << "'  is " << res << endl;
+    return 0;
+}
